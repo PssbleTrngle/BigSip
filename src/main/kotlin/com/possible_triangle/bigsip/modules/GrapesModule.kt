6f6f45b49a3b7c1dea65ae89b04d5810aaede9c0
PssbleTrngle <nik@somethingcatchy.net>
2022-08-01 10:18:40 +0200

    correct apple juice file name

--- src/main/kotlin/com/possible_triangle/bigsip/modules/GrapesModule.kt
+++ src/main/kotlin/com/possible_triangle/bigsip/modules/GrapesModule.kt
@@ -4,6 +4,7 @@ import com.possible_triangle.bigsip.Registration
 import com.possible_triangle.bigsip.block.GrapeCrop
 import com.possible_triangle.bigsip.data.generation.TagBuilder
 import com.possible_triangle.bigsip.data.generation.recipes.RecipeBuilder
+import net.minecraft.world.food.Foods
 import net.minecraft.world.item.Item
 import net.minecraft.world.item.ItemNameBlockItem
 import net.minecraftforge.common.Tags
@@ -11,7 +12,7 @@ import thedarkcolour.kotlinforforge.forge.registerObject
 
 object GrapesModule : ModModule {
 
-    val GRAPES by Registration.ITEMS.registerObject("grapes") { Item(Registration.Properties) }
+    val GRAPES by Registration.ITEMS.registerObject("grapes") { Item(Registration.Properties.food(Foods.APPLE)) }
     val GRAPES_TAG = Registration.fruitTag("grapes")
     val GRAPE_CROP by Registration.BLOCKS.registerObject("grapes") { GrapeCrop() }
     val GRAPE_SAPLING by Registration.ITEMS.registerObject("grape_sapling") {
