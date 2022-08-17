2e9eddcfd42416d9d571a963447233fc2110837c
PssbleTrngle <nik@somethingcatchy.net>
2022-08-17 12:24:59 +0200

    update to create version 0.5.0d

--- src/main/kotlin/com/possible_triangle/bigsip/compat/jei/JEIPlugin.kt
+++ src/main/kotlin/com/possible_triangle/bigsip/compat/jei/JEIPlugin.kt
@@ -2,27 +2,21 @@ package com.possible_triangle.bigsip.compat.jei
 
 import com.possible_triangle.bigsip.BigSip
 import com.possible_triangle.bigsip.Registration
-import com.possible_triangle.bigsip.modules.MaturingModule
 import com.possible_triangle.bigsip.modules.ModModule
 import com.possible_triangle.bigsip.modules.ServerLoadingContext
-import com.possible_triangle.bigsip.recipe.MaturingRecipe
-import com.simibubi.create.compat.jei.category.CreateRecipeCategory
 import mezz.jei.api.IModPlugin
 import mezz.jei.api.JeiPlugin
 import mezz.jei.api.constants.VanillaTypes
+import mezz.jei.api.forge.ForgeTypes
 import mezz.jei.api.ingredients.subtypes.UidContext
 import mezz.jei.api.registration.IRecipeCatalystRegistration
 import mezz.jei.api.registration.IRecipeCategoryRegistration
 import mezz.jei.api.registration.IRecipeRegistration
 import mezz.jei.api.registration.ISubtypeRegistration
 import mezz.jei.api.runtime.IJeiRuntime
-import net.minecraft.client.Minecraft
 import net.minecraft.resources.ResourceLocation
-import net.minecraft.world.Container
 import net.minecraft.world.item.BucketItem
 import net.minecraft.world.item.ItemStack
-import net.minecraft.world.item.crafting.Recipe
-import net.minecraft.world.item.crafting.RecipeType
 import net.minecraftforge.fluids.FluidStack
 import net.minecraftforge.server.ServerLifecycleHooks
 
@@ -30,37 +24,23 @@ import net.minecraftforge.server.ServerLifecycleHooks
 @JeiPlugin
 class JEIPlugin : IModPlugin {
 
-    override fun getPluginUid(): ResourceLocation {
-        return ResourceLocation(BigSip.MOD_ID, BigSip.MOD_ID)
-    }
+    override fun getPluginUid() = ResourceLocation(BigSip.MOD_ID, "jei")
 
     override fun registerCategories(registration: IRecipeCategoryRegistration) {
         registration.addRecipeCategories(MaturingCategory)
     }
 
     override fun registerRecipes(registration: IRecipeRegistration) {
-        val minecraft = Minecraft.getInstance()
-        val world = minecraft.level!!
-
-        fun <C : Container, T : Recipe<C>> IRecipeRegistration.addRecipes(
-            category: CreateRecipeCategory<T>,
-            type: RecipeType<T>,
-        ) {
-            val recipes = world.recipeManager.getAllRecipesFor(type)
-            addRecipes(category.recipeType, recipes)
-        }
-
-        registration.addRecipes(MaturingCategory, MaturingRecipe.INFO.getType())
-
+        MaturingCategory.registerRecipes(registration)
     }
 
     override fun registerRecipeCatalysts(registration: IRecipeCatalystRegistration) {
-        registration.addRecipeCatalyst(ItemStack(MaturingModule.BARREL_ITEM), MaturingCategory.recipeType)
+        MaturingCategory.registerCatalysts(registration)
     }
 
     override fun registerItemSubtypes(registration: ISubtypeRegistration) {
         Registration.DRINKS.forEach {
-            registration.registerSubtypeInterpreter(it) { stack, ctx ->
+            registration.registerSubtypeInterpreter(VanillaTypes.ITEM_STACK, it) { stack, ctx ->
                 if (ctx == UidContext.Recipe) ""
                 else stack.damageValue.toString()
             }
@@ -75,18 +55,18 @@ class JEIPlugin : IModPlugin {
         val hiddenItems = ModModule.hiddenItems(context)
 
         hiddenItems.map(::ItemStack).takeIf { it.isNotEmpty() }?.let { hidden ->
-            runtime.ingredientManager.removeIngredientsAtRuntime(VanillaTypes.ITEM, hidden)
+            runtime.ingredientManager.removeIngredientsAtRuntime(VanillaTypes.ITEM_STACK, hidden)
         }
 
         val buckets = Registration.ITEMS.entries.map { it.get() }.filterIsInstance<BucketItem>()
         hiddenFluids.takeIf { it.isNotEmpty() }?.let { hidden ->
             val stacks = hidden.map { FluidStack(it, 1) }
-            runtime.ingredientManager.removeIngredientsAtRuntime(VanillaTypes.FLUID, stacks)
+            runtime.ingredientManager.removeIngredientsAtRuntime(ForgeTypes.FLUID_STACK, stacks)
         }
 
-        val hiddenBuckets = buckets.filter {  hiddenFluids.contains(it.fluid) }.map(::ItemStack)
+        val hiddenBuckets = buckets.filter { hiddenFluids.contains(it.fluid) }.map(::ItemStack)
         if (hiddenBuckets.isNotEmpty()) {
-            runtime.ingredientManager.removeIngredientsAtRuntime(VanillaTypes.ITEM, hiddenBuckets)
+            runtime.ingredientManager.removeIngredientsAtRuntime(VanillaTypes.ITEM_STACK, hiddenBuckets)
         }
     }
 
--- src/main/kotlin/com/possible_triangle/bigsip/compat/jei/MaturingCategory.kt
+++ src/main/kotlin/com/possible_triangle/bigsip/compat/jei/MaturingCategory.kt
@@ -6,42 +6,54 @@ import com.possible_triangle.bigsip.BigSip.MOD_ID
 import com.possible_triangle.bigsip.modules.GrapesModule
 import com.possible_triangle.bigsip.modules.MaturingModule
 import com.possible_triangle.bigsip.recipe.MaturingRecipe
+import com.simibubi.create.compat.jei.DoubleItemIcon
+import com.simibubi.create.compat.jei.EmptyBackground
 import com.simibubi.create.compat.jei.category.CreateRecipeCategory
 import com.simibubi.create.compat.jei.category.animations.AnimatedKinetics
 import com.simibubi.create.foundation.gui.AllGuiTextures
 import com.simibubi.create.foundation.gui.element.GuiGameElement
-import mezz.jei.api.constants.VanillaTypes
+import mezz.jei.api.forge.ForgeTypes
 import mezz.jei.api.gui.builder.IRecipeLayoutBuilder
 import mezz.jei.api.gui.ingredient.IRecipeSlotsView
 import mezz.jei.api.recipe.IFocusGroup
 import mezz.jei.api.recipe.RecipeIngredientRole
 import mezz.jei.api.recipe.RecipeType
 import net.minecraft.client.Minecraft
-import net.minecraft.network.chat.Component
 import net.minecraft.network.chat.TranslatableComponent
+import net.minecraft.world.item.ItemStack
+import net.minecraft.world.level.ItemLike
+import java.util.function.Supplier
 import kotlin.time.Duration.Companion.milliseconds
 
-object MaturingCategory : CreateRecipeCategory<MaturingRecipe>(doubleItemIcon(MaturingModule.BARREL_ITEM, GrapesModule.GRAPES),
-    emptyBackground(177, 70)) {
+private fun supplierStack(item: ItemLike): Supplier<ItemStack> {
+    return Supplier { ItemStack(item) }
+}
 
-    override fun getRecipeClass() = MaturingRecipe::class.java
+private fun getRecipes(): List<MaturingRecipe> {
+    val connection = Minecraft.getInstance().connection ?: return emptyList()
+    return connection.recipeManager.getAllRecipesFor(MaturingRecipe.INFO.getType())
+}
 
-    init {
-        name = MaturingRecipe.ID
-        type = RecipeType.create(MOD_ID, name, recipeClass)
-    }
-
-    override fun getTitle(): Component = TranslatableComponent("$MOD_ID.recipe.$name")
+object MaturingCategory : CreateRecipeCategory<MaturingRecipe>(
+    Info(
+        RecipeType.create(MOD_ID, MaturingRecipe.ID, MaturingRecipe::class.java),
+        TranslatableComponent("$MOD_ID.recipe.${MaturingRecipe.ID}"),
+        EmptyBackground(177, 70),
+        DoubleItemIcon(supplierStack(MaturingModule.BARREL_ITEM), supplierStack(GrapesModule.GRAPES)),
+        ::getRecipes,
+        listOf(supplierStack(MaturingModule.BARREL_ITEM))
+    )
+) {
 
     override fun setRecipe(builder: IRecipeLayoutBuilder, recipe: MaturingRecipe, focuses: IFocusGroup) {
         val input = recipe.fluidIngredients.getOrNull(0) ?: return
 
         builder.addSlot(RecipeIngredientRole.INPUT, 27, 36).setBackground(getRenderedSlot(), -1, -1)
-            .addIngredients(VanillaTypes.FLUID, input.getMatchingFluidStacks())
+            .addIngredients(ForgeTypes.FLUID_STACK, input.getMatchingFluidStacks())
             .addTooltipCallback(addFluidTooltip(MaturingRecipe.DISPLAY_AMOUNT))
 
         builder.addSlot(RecipeIngredientRole.OUTPUT, 132, 36).setBackground(getRenderedSlot(), -1, -1)
-            .addIngredients(VanillaTypes.FLUID, recipe.fluidResults)
+            .addIngredients(ForgeTypes.FLUID_STACK, recipe.fluidResults)
             .addTooltipCallback(addFluidTooltip(MaturingRecipe.DISPLAY_AMOUNT))
     }
 
